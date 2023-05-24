3243c462701d463cdcffa03f302a38f145115594
troingle <109359272+troingle@users.noreply.github.com>
2023-05-24 10:00:53 +0300

    Added moves, weapon list and started combat system

--- JavascriptRPG/javascriptRpg.js
+++ JavascriptRPG/javascriptRpg.js
@@ -1,14 +1,49 @@
+var gold = 0;
 
+weapons = ["Bare fists", "Club", "Sword", "Mace", "Excalibur", "Meteor hammer", "Jim's hand", "Hell blade idk I'm running out of ideas"];
+weaponNum = 0;
+
+enemyList = []
 
 function nextMove(){
 
-     input = prompt("What is your input?", "");
+    var input = prompt("1) Walk forwards 2) Go to shop 3) Check stats 4) Make sacrifice", "");
 
+    errorMessage = "";
+
+    if (input == "1"){
+        walkForwards()
+    }
+    else if (input == "2"){
+        goShop()
+    }
+    else if (input == "3"){
+        checkStats()
+    }
+    else if (input == "4"){
+        makeSacrifice()
+    }
+    else{
+        alert("Please choose a number between 1 and 4.")
+        nextMove()
+    }
 }
 
-var input = prompt("Welcome! To play the game, type the number that corresponds to your choice of action. 1) Proceed 2) Leave")
+function walkForwards(){
+    var chosenEnemy = enemyList[Math.floor(Math.random() * 5) + 1]
+    alert("You encounter the enemy " + chosenEnemy)
+}
 
-if (input == "1"){
+function goShop(){
+    alert("Welcome to the shop! What would you like to buy?")
+    input = prompt("1) ", "")
+}
+
+function checkStats(){
+    alert("Gold: " + gold + "\nWeapon: " + weapons[weaponNum])
     nextMove()
 }
 
+alert("Welcome! To play the game, type the number that corresponds to your choice of action.")
+
+nextMove()
